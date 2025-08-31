da22dab1b01c8710b4cc6e012c777c3ca5040952
azsgit <10238943+azsgit@users.noreply.github.com>
2025-09-01 02:18:37 +0900

    wip

--- manage/bin/create.sh
+++ manage/bin/create.sh
@@ -18,35 +18,41 @@ function getmdl {
     if cnfrtn "import: ${mdltgt}: ${mdlurl}"
     then
       mkdir "${pthapp}"/mdl/${mdltgt}
-      cd "${pthapp}"/mdl/${mdltgt}
-      for i in "${lstrsc[@]}"
-      do
-        curl -LO "${i}"
-      done
+      if cd "${pthapp}"/mdl/${mdltgt}
+      then
+        for i in "${lstrsc[@]}"
+        do
+          curl -LO "${i}"
+        done
+      fi
     fi
   fi
 }
 
 if ! docker inspect ${imgtgt} > /dev/null 2>&1
 then
-  cd ${pthdoc}
-  test -e ${txtcns} || curl https://raw.githubusercontent.com/litagin02/Style-Bert-VITS2/master/requirements.txt > ${txtcns}
+  if cd ${pthdoc}
+  then
+    test -e ${txtcns} || curl https://raw.githubusercontent.com/litagin02/Style-Bert-VITS2/master/requirements.txt > ${txtcns}
+  fi
 fi
 
 addimg ${imgtgt} "${cnfimg}" "${pthdoc}"
 test -d "${pthapp}" || mkdir "${pthapp}"
-cd "${pthapp}"
-test -d hgf || mkdir hgf
-test -d mdl || mkdir mdl
+if cd "${pthapp}"
+then
+  test -d hgf || mkdir hgf
+  test -d mdl || mkdir mdl
 
-getmdl amitaro \
-https://huggingface.co/litagin/sbv2_amitaro/tree/main/amitaro \
-https://huggingface.co/litagin/sbv2_amitaro/resolve/main/amitaro/amitaro.safetensors \
-https://huggingface.co/litagin/sbv2_amitaro/resolve/main/amitaro/config.json \
-https://huggingface.co/litagin/sbv2_amitaro/resolve/main/amitaro/style_vectors.npy
+  getmdl amitaro \
+  https://huggingface.co/litagin/sbv2_amitaro/tree/main/amitaro \
+  https://huggingface.co/litagin/sbv2_amitaro/resolve/main/amitaro/amitaro.safetensors \
+  https://huggingface.co/litagin/sbv2_amitaro/resolve/main/amitaro/config.json \
+  https://huggingface.co/litagin/sbv2_amitaro/resolve/main/amitaro/style_vectors.npy
 
-getmdl Anneli \
-https://huggingface.co/kaunista/kaunista-style-bert-vits2-models/tree/main/Anneli \
-https://huggingface.co/kaunista/kaunista-style-bert-vits2-models/resolve/main/Anneli/Anneli_e116_s32000.safetensors \
-https://huggingface.co/kaunista/kaunista-style-bert-vits2-models/resolve/main/Anneli/config.json \
-https://huggingface.co/kaunista/kaunista-style-bert-vits2-models/resolve/main/Anneli/style_vectors.npy
+  getmdl Anneli \
+  https://huggingface.co/kaunista/kaunista-style-bert-vits2-models/tree/main/Anneli \
+  https://huggingface.co/kaunista/kaunista-style-bert-vits2-models/resolve/main/Anneli/Anneli_e116_s32000.safetensors \
+  https://huggingface.co/kaunista/kaunista-style-bert-vits2-models/resolve/main/Anneli/config.json \
+  https://huggingface.co/kaunista/kaunista-style-bert-vits2-models/resolve/main/Anneli/style_vectors.npy
+fi
